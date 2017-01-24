ef6a5c2e6d234bba85ef7e9cd6c63e3131577bc5
DaveDLeague <dave.dunn@jointheleague.org>
2017-01-24 13:16:17 -0800

    Update ArrayUtil.java

--- src/ArrayUtil.java
+++ src/ArrayUtil.java
@@ -1,4 +1,10 @@
 
+/** Reverse elements of array arr
+*   Precondition: arr.length > 0.
+*   Postcondition: The elements of arr have been reversed
+*   @param arr the array to manipulate
+*/
+
 public class ArrayUtil {
 	public static void reverseArray(int[] arr){
 		/* code goes here */
@@ -12,10 +18,22 @@ class Matrix{
 		mat = m;
 	}
 	
+	/** Revereses the elements in each row of mat.
+	*   Postcondition: The elements in each row have been reversed
+	*/
 	public void reverseAllRows(){
 		/* code goes here */
 	}
 	
+	/** Reverses the elements of mat.
+	*   Postcondition:
+	*   - The final elements of mat, when read in row-major order, 
+	*     are the same as the original elements of mat when read
+	*     from the bottom corner, right to left, going upward.
+	*   - mat[0][0] contains what was originally the last element.
+	*   - mat[mat.length - 1][mat[0].length -1] contains what was
+	*     originally the first element.
+	*/
 	public void reverseMatrix(){
 		/* code goes here */
 	}
